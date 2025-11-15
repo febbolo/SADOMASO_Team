0b61f50f742c8916b6d125d7e0c9503b860ec225
utente <utente@DESKTOP-QG5NTUN>
2025-11-15 14:43:55 +0100

    Creato file per aggiornare libreria in automatico modificato nomi nei file

---
+++ Aggiorna_library.m
@@ -0,0 +1,28 @@
+%% PER AGGIORNARE LA LIBRERIA
+
+% nuove versioni usando Source e non Reference, attento brodo
+% basta cambiare old e new in base ai nomi che salvano
+
+% ho 
+
+open_system('sim_lab7_disturbances.slx')
+
+oldLib = 'LIBRARY';
+newLib = 'LIBRARY_SAD';
+
+blks = find_system(gcs, 'LookUnderMasks','all','FollowLinks','on');
+
+for i = 1:numel(blks)
+    try
+        src = get_param(blks{i}, 'SourceBlock');
+
+        % Deve iniziare per 'LIBRARY/'
+        if startsWith(src, [oldLib '/'])
+            % Rimpiazza SOLO il prefisso iniziale
+            newSrc = [newLib src(length(oldLib)+1:end)];
+            set_param(blks{i}, 'SourceBlock', newSrc);
+        end
+
+    catch
+    end
+end

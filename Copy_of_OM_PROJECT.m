8670b9f8923a2a540fbd0b31f22fb649288de70f
delil <delil@DESKTOP-OJUOTVK>
2025-12-13 23:54:41 +0100

    non mi fa aggiungere il file di orbital da modificare

---
+++ Copy_of_OM_PROJECT.m
@@ -0,0 +1,24 @@
+clear
+close all
+clc
+
+% data from Lab Assignement
+
+% Group number: 2519
+% Mars to Earth 
+% Asteroid N.470864 
+% 00:00:00 01/01/2030 
+% 00:00:00 01/01/2060 
+
+
+%2519 
+% Central planet Earth 
+%  a = 2.4979 
+%  e = 0.5162 
+%  i = 59
+%  w = 43 
+%  Raan = 53 
+%  k = 1
+%  m = 5
+%  Perturbations: J2 Moon 
+%  Initial Date: 2041-6-28-22-40-59

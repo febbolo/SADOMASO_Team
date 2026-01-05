51c27d69a055961488b6509b8fac3c356de804a8
Lucantropo <luca.deli@mail.polimi.it>
2026-01-05 16:36:45 +0100

    ultimate fixes: removed scopes, commented parts, and changed angular velocities

--- SAD_PROJECT/SCRIPT_SAD_DEMO.m
+++ SAD_PROJECT/SCRIPT_SAD_DEMO.m
@@ -674,120 +674,120 @@ ylabel('\Delta\theta_{point} [deg]');
 grid on;
 
 
-%% ---------- MONTE CARLO ANALYSIS ----------
-
-N_MC = 50;
-
-% Preallocate results
-% w_MC(sim, axis, time)
-w_MC = cell(N_MC,1);
-time_MC = cell(N_MC,1);
-
-% Save nominal values
-J_nom   = J_depl;
-w0_nom  = w0;
-a_nom   = a;
-e_nom   = e;
-incl_nom = incl;
-raan_nom = raan;
-SRP_nom = SRP;
-
-for k = 1:N_MC
-
-    fprintf('Monte Carlo run %d / %d \n', k, N_MC);
-
-    % Random seed
-    rng(k);
-    base_seed_MC = base_seed;
-    base_seed = base_seed_MC + k; 
-
-    % INERTIA (±10% sui principali, ±5% cross)
-    J_depl = J_nom .* diag(1 + 0.10*randn(3,1));
-
-    % ANGULAR VELOCITIES ±30%
-    w0 = w0_nom .* (1 + 0.30*randn(3,1));
-
-    % ORBITS UNCERTAINTIES
-    a     = a_nom    * (1 + 0.005*randn);     % ±0.5%
-    e     = max(0, e_nom + 0.001*randn);      % small variation
-    incl  = incl_nom + 0.1*randn;             % ±0.1 deg
-    raan  = raan_nom + 0.2*randn;             % ±0.2 deg
-
-    kep = [a,e,incl,raan,w,theta];
-
-    % SRP DISTURBANCES UNCERTAINTY
-    fields = fieldnames(SRP_nom);
-    for ii = 1:length(fields)
-        SRP.(fields{ii}).A = SRP_nom.(fields{ii}).A * (1 + 0.05*randn);
-        SRP.(fields{ii}).r_F = SRP_nom.(fields{ii}).r_F .* (1 + 0.10*randn(1,3));
-    end
-
-    % Simulation
-    simout = sim('SIM_SAD_DEMO','FastRestart','off');
-
-    % Saving results
-    w_MC{k} = simout.w_B;
-    time_MC{k} = simout.tout;    
-end
-
-% Creating matrix for angular velocities
-Nt = length(time_MC{1});
-w_MC_mat = zeros(N_MC,3,Nt);
-for k = 1:N_MC
-    w_MC_mat(k,:,:) = rad2deg(w_MC{k});
-end
-
-%% --------- PLOT : MONTE CARLO ANALYSIS ---------- 
-
-figure('Name','Monte Carlo Angular Velocities');
-hold on;
-N_MC = size(w_MC_mat,1);
-Nt   = size(w_MC_mat,3);
-t = time_MC{1};
-for k = 1:N_MC
-    plot(t, squeeze(w_MC_mat(k,1,:)),'LineWidth',1); % w_x
-    plot(t, squeeze(w_MC_mat(k,2,:)),'LineWidth',1); % w_y
-    plot(t, squeeze(w_MC_mat(k,3,:)),'LineWidth',1); % w_z
-end
-xlabel('Time [s]');
-ylabel('Angular velocity [deg/s]');
-title('Monte Carlo Angular Velocities (w_x, w_y, w_z)');
-grid on;
-legend({'\omega_x','\omega_y','\omega_z'});
-hold off;
-
-
-% Restoring nominal values
-J_depl = J_nom;
-w0     = w0_nom;
-a      = a_nom;
-e      = e_nom;
-incl   = incl_nom;
-raan   = raan_nom;
-SRP    = SRP_nom;
-
-
-
-%% ----------- SATELLITE SCENARIO ----------------
-
-%Conversion a km -> m
-a = a*1000;         %[m]
-
-%Satellite Scenario 
-stopTime = startTime + seconds(2*T);
-sampleTime = 60;
-sc = satelliteScenario(startTime,stopTime,sampleTime);
-viewer = satelliteScenarioViewer(sc,"CameraReferenceFrame","Inertial","Dimension","3D");
-sat = satellite(sc,a,e,incl,raan,w,theta);
-show(sat)
-sat.Visual3DModel = "SmallSat.glb";
-coordinateAxes(sat, Scale=2); % red = x_B; green = y_B; blue = z_B
-camtarget(viewer, sat);
-groundTrack(sat,"LeadTime",3600,"LeadLineColor",[0 1 0],"TrailLineColor",[0 1 0]);
-play(sc,PlaybackSpeedMultiplier=500)
-
-%Conversion a m -> km
-a = a/1000;  %[km] 
+% %% ---------- MONTE CARLO ANALYSIS ----------
+% 
+% N_MC = 50;
+% 
+% % Preallocate results
+% % w_MC(sim, axis, time)
+% w_MC = cell(N_MC,1);
+% time_MC = cell(N_MC,1);
+% 
+% % Save nominal values
+% J_nom   = J_depl;
+% w0_nom  = w0;
+% a_nom   = a;
+% e_nom   = e;
+% incl_nom = incl;
+% raan_nom = raan;
+% SRP_nom = SRP;
+% 
+% for k = 1:N_MC
+% 
+%     fprintf('Monte Carlo run %d / %d \n', k, N_MC);
+% 
+%     % Random seed
+%     rng(k);
+%     base_seed_MC = base_seed;
+%     base_seed = base_seed_MC + k; 
+% 
+%     % INERTIA (±10% sui principali, ±5% cross)
+%     J_depl = J_nom .* diag(1 + 0.10*randn(3,1));
+% 
+%     % ANGULAR VELOCITIES ±30%
+%     w0 = w0_nom .* (1 + 0.30*randn(3,1));
+% 
+%     % ORBITS UNCERTAINTIES
+%     a     = a_nom    * (1 + 0.005*randn);     % ±0.5%
+%     e     = max(0, e_nom + 0.001*randn);      % small variation
+%     incl  = incl_nom + 0.1*randn;             % ±0.1 deg
+%     raan  = raan_nom + 0.2*randn;             % ±0.2 deg
+% 
+%     kep = [a,e,incl,raan,w,theta];
+% 
+%     % SRP DISTURBANCES UNCERTAINTY
+%     fields = fieldnames(SRP_nom);
+%     for ii = 1:length(fields)
+%         SRP.(fields{ii}).A = SRP_nom.(fields{ii}).A * (1 + 0.05*randn);
+%         SRP.(fields{ii}).r_F = SRP_nom.(fields{ii}).r_F .* (1 + 0.10*randn(1,3));
+%     end
+% 
+%     % Simulation
+%     simout = sim('SIM_SAD_DEMO','FastRestart','off');
+% 
+%     % Saving results
+%     w_MC{k} = simout.w_B;
+%     time_MC{k} = simout.tout;    
+% end
+% 
+% % Creating matrix for angular velocities
+% Nt = length(time_MC{1});
+% w_MC_mat = zeros(N_MC,3,Nt);
+% for k = 1:N_MC
+%     w_MC_mat(k,:,:) = rad2deg(w_MC{k});
+% end
+% 
+% %% --------- PLOT : MONTE CARLO ANALYSIS ---------- 
+% 
+% figure('Name','Monte Carlo Angular Velocities');
+% hold on;
+% N_MC = size(w_MC_mat,1);
+% Nt   = size(w_MC_mat,3);
+% t = time_MC{1};
+% for k = 1:N_MC
+%     plot(t, squeeze(w_MC_mat(k,1,:)),'LineWidth',1); % w_x
+%     plot(t, squeeze(w_MC_mat(k,2,:)),'LineWidth',1); % w_y
+%     plot(t, squeeze(w_MC_mat(k,3,:)),'LineWidth',1); % w_z
+% end
+% xlabel('Time [s]');
+% ylabel('Angular velocity [deg/s]');
+% title('Monte Carlo Angular Velocities (w_x, w_y, w_z)');
+% grid on;
+% legend({'\omega_x','\omega_y','\omega_z'});
+% hold off;
+% 
+% 
+% % Restoring nominal values
+% J_depl = J_nom;
+% w0     = w0_nom;
+% a      = a_nom;
+% e      = e_nom;
+% incl   = incl_nom;
+% raan   = raan_nom;
+% SRP    = SRP_nom;
+% 
+% 
+% 
+% %% ----------- SATELLITE SCENARIO ----------------
+% 
+% %Conversion a km -> m
+% a = a*1000;         %[m]
+% 
+% %Satellite Scenario 
+% stopTime = startTime + seconds(2*T);
+% sampleTime = 60;
+% sc = satelliteScenario(startTime,stopTime,sampleTime);
+% viewer = satelliteScenarioViewer(sc,"CameraReferenceFrame","Inertial","Dimension","3D");
+% sat = satellite(sc,a,e,incl,raan,w,theta);
+% show(sat)
+% sat.Visual3DModel = "SmallSat.glb";
+% coordinateAxes(sat, Scale=2); % red = x_B; green = y_B; blue = z_B
+% camtarget(viewer, sat);
+% groundTrack(sat,"LeadTime",3600,"LeadLineColor",[0 1 0],"TrailLineColor",[0 1 0]);
+% play(sc,PlaybackSpeedMultiplier=500)
+% 
+% %Conversion a m -> km
+% a = a/1000;  %[km] 
 
 
 
